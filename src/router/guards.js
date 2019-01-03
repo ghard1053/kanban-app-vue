cea41bc7b45143467d41df2e166b3df3fbc1f727
Takao <sukisya70@gmail.com>
2019-01-03 23:01:28 +0900

    開発サーバー

--- src/router/guards.js
+++ src/router/guards.js
@@ -1,6 +1,6 @@
 import store from '../store'
 
-export const authorizeToken = (to, from , next) => {
+export const authorizeToken = (to, from, next) => {
   if (to.matched.some(record => record.meta.requiresAuth)) {
     if (!store.state.auth || !store.state.auth.token) {
       next({ path: '/login' })
